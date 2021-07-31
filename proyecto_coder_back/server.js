ce373dbfb318feeab981fba70890628b6f878036
Tomas <tomasftrotti@gmail.com>
2021-07-31 11:05:01 -0300

    further improvements towards passport-local integration from signup and login

--- proyecto_coder_back/server.js
+++ proyecto_coder_back/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors'
+import passport from 'passport'
 import logger from './lib/logger.js'
 
 import productRouter from './routers/prodRouter.js'
@@ -13,6 +14,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api/carrito', cartRouter);
 app.use('/api/productos', productRouter);
